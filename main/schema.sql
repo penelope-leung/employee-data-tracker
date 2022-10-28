42ddcf4e906cdbb4d827f59141df4bcc3ce2ed1e
PenPen812 <penelope.c.leung@gmail.com>
2022-10-28 16:02:58 -0500

    added code on schema.sql

---
+++ main/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS department_db;
+CREATE DATABASE department_db;
+
+USE department_db;
+
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  employee_name VARCHAR(30) NOT NULL,
+ 
+);
+
+CREATE TABLE employees_role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL AUTO_INCREMENT FOREIGN KEY, 
+  manager_id INT NOT NULL AUTO_INCREMENT FOREIGN KEY,
+  FOREIGN KEY (employee_id) REFERENCES employees_role(id) ON DELETE SET NULL
+  FOREIGN KEY (manager_id) REFERENCES manager(id) ON DELETE SET NULL
+);
